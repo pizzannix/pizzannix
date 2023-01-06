d453958ba8b8192b6d4b40667117e3f156c682e9
pizzannix <110530957+pizzannix@users.noreply.github.com>
2023-01-07 00:27:20 +0300

    kali.py
    installation za kali tools

---
+++ new kali.py
@@ -0,0 +1,17 @@
+katoolin
+Automatically install all Kali linux tools
+
+Features
+Add Kali linux repositories
+Remove kali linux repositories
+Install Kali linux tools
+Requirements
+Python 2.7
+An operating system (tested on Ubuntu)
+Installation
+▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️
+
+sudo su
+git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
+chmod +x /usr/bin/katoolin
+sudo katoolin
